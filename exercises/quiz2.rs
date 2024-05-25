b7e6968209160cdf827a3ed7336b812a60496283
weijia cheng <646025053@qq.com>
2024-05-25 13:28:36 +0000

    quiz2 over

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -32,11 +32,24 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String,Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            let temp =match command{
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(times) => {
+                    let mut result = string.clone();
+                    for _ in 0..*times as i32 {
+                        result.push_str("bar");
+                    }
+                    result
+                }
+                
+            };
+            output.push(temp);
         }
         output
     }
@@ -45,9 +58,9 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+  
     use super::Command;
-
+    use crate::my_module::transformer;
     #[test]
     fn it_works() {
         let output = transformer(vec![
