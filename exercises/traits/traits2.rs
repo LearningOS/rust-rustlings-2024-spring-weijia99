e5ed974dc3c991a1573d62bf656dc0247a57fa33
weijia cheng <646025053@qq.com>
2024-05-26 04:23:00 +0000

    quiz3 over

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -8,13 +8,25 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
-// TODO: Implement trait `AppendBar` for a vector of strings.
+
+impl AppendBar for Vec<String>{
+
+    fn append_bar(self) -> Self{
+        let mut v = Vec::new();
+        for it in &self{
+            v.push(it.to_owned());
+        }
+        v.push("Bar".to_string());
+        v
+
+    }
+}
 
 #[cfg(test)]
 mod tests {
